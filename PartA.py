88793b843436c474321913eb80bbf90aac045d2b
Jack0Shea <158283503+Jack0Shea@users.noreply.github.com>
2024-02-06 11:46:31 +0000

    Add files via upload

---
+++ PartA.py
@@ -0,0 +1,14 @@
+#Jack O'Shea
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps():
+    for i in range (a, b+i):
+        print (f"Steps {i}")
+    
+print("Step 1")
+print("Step 2")
+printSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
---
+++ PartB.py
@@ -0,0 +1,10 @@
+#Jack O'Shea
+#30/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps():
+    for i in range (a, b+i):
+        print (f"Steps {i}")
+    
+
+printSteps(1,9)
